4cbc5896f2c6c087d3dfb31b6b59dcfcd86384d7
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-01-05 15:16:07 +0100

    Create new function for computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hi!")
+
+//Creating the three choice options as an array and store in a variable
+let choice = ["rock", "paper", "scissors"]
+
+
+
+// This function randomly chooses one of the three options from array "choice" and returns the choice
+function getComputerChoice(choice) {
+    return choice[Math.floor(Math.random()*choice.length)]
+}
+
+
+// The random choice from function "getComputerSelection" is stores in a new variable
+let computerSelection = console.log(getComputerChoice(choice));
+

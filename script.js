19917200109981883f7a4cc0cd8e730eff63b07f
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-01-09 14:38:14 +0100

    Fix bug concerning misspelling in func playRound

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 
 //Create the three choice options as an array and store in a variable
-const choice = ["rock", "paper", "scissors"]
+const choice = ["rock", "paper", "scissors"];
 
 // Create prompt for player's choice and make case-insensitive
-let PlayerChoice = prompt("Choose \"rock\", \"paper\" or \"scissors\"").toLowerCase();
-let playerSelection = PlayerChoice.toLowerCase();
-console.log(playerSelection);
+let playerSelection = prompt("Choose \"rock\", \"paper\" or \"scissors\"").toLowerCase();
+console.log("Player chose: " + playerSelection);
 
 
 // Function randomly chooses one of the three options from array and returns the choice
 function getComputerChoice(choice) {
-    return choice[Math.floor(Math.random()*choice.length)]
-}
+    return choice[Math.floor(Math.random()*choice.length)];
+};
 
 
 // The random choice from function is stored in a new variable
 let computerSelection = getComputerChoice(choice);
-console.log(computerSelection);
+console.log("Computer chose: " + computerSelection);
+
+// These variables will store the current score for player and computer and keep track of how many rounds have been played
+let playerScore = 0;
+let computerScore = 0;
+let whichRound = 1;
 
 
-// This function finds the result of one game and returns the result, new score and round no.
+// This function finds the result of one game and returns the result, round number and new score
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === choice[0]) && (computerSelection === choice[1])) {
         return `You loose! ${choice[1]} beats ${choice[0]}. After round ${whichRound}, the score is computer ${++computerScore} : ${playerScore} player`;
@@ -33,7 +37,7 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === computerSelection) {
         return `It is a tie! Try again. After round ${whichRound}, the score is computer ${computerScore} : ${playerScore} player`;
     }
-    else if (playerSelection !== choice[0, 1, 2]) {
+    else if ((playerSelection !== choice[0]) && (playerSelection !== choice[1]) && (playerSelection !== choice[2])) {
         return `You misspelled your choice. Please try again. After round ${whichRound}, the score is computer ${computerScore} : ${playerScore} player`;
     }
     else {
@@ -42,35 +46,23 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-// These variables will store the current score for player and computer and keep track of how many rounds have been played
-let playerScore = 0;
-let computerScore = 0;
-let whichRound = 1;
-let keepGoing = true;
-
 // This following loop and functions will run the game for a defined number of cycles/games
 for (let i = 0; i < 5; i++) {   
-    function game(keepGoing) { 
+    
+    function game() {
         console.log(playRound(playerSelection, computerSelection));
-        ++whichRound
+        ++whichRound;
+    }
         if ((whichRound === 5) && (computerScore > playerScore)) {
-            keepGoing = false;
-            return "Sorry, the computer won :-(";
+            console.log("Sorry, the computer won :-(")
         }
         else if ((whichRound === 5) && (computerScore < playerScore)) {
-            keepGoing = false;
-            return "Congratulations! You won the game."
+            console.log("Congratulations! You won the game.")
         }
         else if ((whichRound === 5) && (computerScore === playerScore)) {
-            keepGoing = false
-            return "It is a tie. Nobody won the game"
+            console.log("It is a tie. Nobody won the game")
         }
-        else {
-            keepGoing;
-        }
-    }
 }
 
-console.log(game());
 
-
+game();
